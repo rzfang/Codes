a3a9cc9efdbaf119a2d272c15e17bd8980eb2f22
RZ Fang <skywine13@gmail.com>
2016-05-08 16:09:02 +0800

    Js Obj adds a new function - Dig.

--- RZ-Js-Obj.js
+++ RZ-Js-Obj.js
@@ -4,30 +4,45 @@
   /* Combine the seconde object into first object.
     'BsObj' = Base Object.
     'ExtObj' = Extend Object.
-    'Md' = Mode, 0: Union mode in default, 1: Intersection mode.
+    'Md' = Mode, 0: always create another object, 1: overwrite the BsObj.
     Return: new object after combined, or null as error.
     Note: this is not perfect. */
   function Combine (BsObj, ExtObj, Md) {
-    var RstObj = {};
+    var RstObj;
 
     if (typeof BsObj !== 'object' || typeof ExtObj !== 'object') { return null; }
 
-    if (typeof Md !== 'number') { Md = 0; }
+    if (!Md) {
+      RstObj = {};
 
-    for (var i in BsObj) { RstObj[i] = BsObj[i]; }
-
-    if (Md === 0) {
-      for (var i in ExtObj) { RstObj[i] = ExtObj[i];}
+      for (var i in BsObj) { RstObj[i] = BsObj[i]; }
     }
-    else {
-      for (var i in ExtObj) {
-        if (typeof RstObj[i] === 'undefined') { continue; }
+    else { RstObj = BsObj; }
 
-        RstObj[i] = ExtObj[i];
-      }
-    }
+    for (var i in ExtObj) { RstObj[i] = ExtObj[i]; }
 
     return RstObj;
+  }
+
+  /* dig the children of given object.
+    'Obj' = Object. the origin object which will be digged.
+    'Pth' = Path. an array records each level of paht.
+    'Flbck' = Fallback. optional, default undefined.
+    Return: sub object, or fallback value. */
+  function Dig (Obj, Pth, Flbck) {
+    var Chld = Obj;
+
+    if (!(Pth instanceof Array) || Pth.length === 0) { return Flbck; }
+
+    if (!Obj || typeof Obj !== 'object') { return Flbck; }
+
+    for (var i = 0; i < Pth.length; i++) {
+      if (!Chld || !Chld.hasOwnProperty(Pth[i])) { return Flbck; }
+
+      Chld = Chld[Pth[i]];
+    }
+
+    return Chld;
   }
 
   /* Merge the seconde object into first object.
@@ -59,6 +74,7 @@
 
   Obj = {
     Combine: Combine,
+    Dig: Dig,
     Merge: Merge
   };
 
